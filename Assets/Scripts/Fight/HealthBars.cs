95ac85fa780f706038946d038b58de5e991fc21f
Anna <anna.pedersen@student.education.wa.edu.au>
2021-09-02 13:37:31 +0800

    health bars

---
+++ Assets/Scripts/Fight/HealthBars.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class HealthBars : MonoBehaviour {
+	
+	[SerializeField]
+	private Image playerSailHealth, playerShipHealth, playerCrewHealth, playerCannonHealth;
+
+	[SerializeField]
+	private Image enemySailHealth, enemyShipHealth, enemyCrewHealth, enemyCannonHealth;
+
+	public void DisplaySailStats(float healthValue) {
+		healthValue /= 100f;
+		playerSailHealth.fillAmount = healthValue;
+	}
+
+	public void DisplayShipStats(float healthValue) {
+		healthValue /= 100f;
+		playerShipHealth.fillAmount = healthValue;
+	}
+
+	public void DisplayCrewStats(float healthValue) {
+		healthValue /= 100f;
+		playerCrewHealth.fillAmount = healthValue;
+	}
+
+	public void DisplayCannonHealth(float healthValue) {
+		healthValue /= 100f;
+		playerCannonHealth.fillAmount = healthValue;
+	}
+
+
+
+	public void DisplayEnemySail(float healthValue) {
+		healthValue /= 100f;
+		enemySailHealth.fillAmount = healthValue;
+	}
+
+	public void DisplayEnemyShip(float healthValue) {
+		healthValue /= 100f;
+		enemyShipHealth.fillAmount = healthValue;
+	}
+
+	public void DisplayEnemyCrew(float healthValue) {
+		healthValue /= 100f;
+		enemyCrewHealth.fillAmount = healthValue;
+	}
+
+	public void DisplayEnemyCannon(float healthValue) {
+		healthValue /= 100f;
+		enemyCannonHealth.fillAmount = healthValue;
+	}
+}
